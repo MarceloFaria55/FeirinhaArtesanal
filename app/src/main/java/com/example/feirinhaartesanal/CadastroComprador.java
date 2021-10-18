57b645331acaf5a213f46c703eb2eaf255716619
MarceloFaria55 <marcelofariadearaujo@gmail.com>
2021-10-18 11:13:18 -0300

    logando

--- app/src/main/java/com/example/feirinhaartesanal/CadastroComprador.java
+++ app/src/main/java/com/example/feirinhaartesanal/CadastroComprador.java
@@ -6,15 +6,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CadastroComprador extends AppCompatActivity {
 
     private EditText caixa_nome,caixa_email,caixa_endereco,caixa_cidade,caixa_UF,caixa_senha,caixa_cpf;
     private Button bt_cadastrar;
-    String[] menssagens = {"Preencha todos os campo", "Cadastro realizado com sucesso"};
+    String[] menssagens = {"Preencha todos os campos", "Cadastro realizado com sucesso", "Deu algum erro"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +48,50 @@ public class CadastroComprador extends AppCompatActivity {
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
                 }
+                else
+                {
+                    CadastrarComprador(v);
+                }
             }
         });
     }
+    private void CadastrarComprador(View v)
+      {
+        String email = caixa_email.getText().toString();
+        String senha = caixa_senha.getText().toString();
 
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
+                {
+                    Snackbar snackbar = Snackbar.make(v, menssagens[1],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+                else
+                {
+                    Snackbar snackbar = Snackbar.make(v, menssagens[2],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+
+            }
+        });
+
+      }
     private void IniciarComponentes()
     {
         caixa_nome = findViewById(R.id.caixa_nome);
         caixa_cidade = findViewById(R.id.caixa_cidade);
         caixa_UF = findViewById(R.id.caixa_UF);
         caixa_cpf = findViewById(R.id.caixa_cpf);
-        caixa_email = findViewById(R.id.caixa_email);
+        caixa_email = findViewById(R.id.caixa_emailC);
         caixa_endereco = findViewById(R.id.caixa_endereco);
-        caixa_senha = findViewById(R.id.caixa_senha);
+        caixa_senha = findViewById(R.id.caixa_senhaC);
         bt_cadastrar = findViewById(R.id.bt_CadastroC);
     }
 }
